850be1d3a2545b0d3e679bf7415cb21936e6bf25
Facundo Saldivia <facundosaldivia@gmail.com>
2021-03-19 15:54:36 -0300

    mobile form fix added

--- src/components/form/styles.js
+++ src/components/form/styles.js
@@ -1,18 +1,25 @@
 import { makeStyles } from "@material-ui/core/styles";
 
-export default makeStyles(() => ({
+export default makeStyles((theme) => ({
   root: {
-    paddingTop: "150px",
+    paddingTop: "80px",
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
+    [theme.breakpoints.up("md")]: {
+      paddingTop: "150px",
+    },
   },
   form: {
-    width: "50%",
+    width: "90%",
     display: "grid",
     gridTemplateColumns: "repeat(2,1fr)",
     rowGap: "100px",
     columnGap: "20px",
+    [theme.breakpoints.up("md")]: {
+      rowGap: "100px",
+      width: "50%",
+    },
   },
   description: { gridColumn: "auto / span 2" },
   button: {
@@ -29,10 +36,13 @@ export default makeStyles(() => ({
     background: "#fefefe",
   },
   alert: {
-    position: "absolute",
-    width: "500px",
-    right: "1rem",
-    top: "85vh",
-    margin: "20px 30px",
+    marginTop: "20px",
+    [theme.breakpoints.up("md")]: {
+      position: "absolute",
+      width: "500px",
+      right: "1rem",
+      top: "85vh",
+      margin: "20px 30px",
+    },
   },
 }));
